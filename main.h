16ef1f4833e524b7a876021cf7ca4080fe19ac1a
VCruz910 <vscruz910@gmail.com>
2022-07-07 17:05:48 +0000

    C - Printf

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*Libraries*/
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+#include <stdout.h>
+
+/*Struct*/
+
+
+/*Prototypes (Mandatory Tasks)*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+/*Prototypes (Vic / Ang Created)*/
+
+
+#endif
