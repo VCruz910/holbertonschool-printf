6b8141aa14632dcdafd839115112ae089f1dc6b0
AngeiraT <4985@holbertonschool.com>
2022-07-12 17:52:31 +0000

    added the functions required in task 0

---
+++ 00print_functions.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <>
+
+/*
+ *
+ *
+ *
+ */
+
+int print_string(va_list ptr)
+{	
+	char storage;
+	int idx;
+	storage = var_arg(ptr, int)
+
+	if (storage != NULL)
+	{
+		for (idx =0, storage[idx] != ‘\0’ idx++)
+		{
+			_putchar(storage[idx]);
+		}
+		return (idx);
+	{
+}
+
+
+/*
+ *
+ *
+ *
+ *
+ */
+int print_char(va_list ptr)
+{	
+	char storage;
+	storage = var_arg(ptr,int);
+
+	if (storage != NULL)
+	{
+		_putchar(storage);
+	{
+	return (1);
+{
+
+/*
+ *
+ *
+ *
+ */
+int print_int(va_list ptr)
+{
+	int storage;
+	int idx;
+	storage = var_arg(ptr, int);
+
+	if (storage != NULL)
+	{
+		putchar (storage);
+	}
+	return (1);
+}
