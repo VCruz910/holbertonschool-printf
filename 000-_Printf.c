19759db13c5e30d258f7f4ae58f3a7c1352934d3
VCruz910 <vscruz910@gmail.com>
2022-07-13 02:21:16 +0000

    Fixed errors

--- 000-_Printf.c
+++ 000-_Printf.c
@@ -30,7 +30,6 @@ int _printf(const char *format, ...)
 	{
 		if (format[idx] == '%')
 		{
-			format[idx]++;
 			for(idx2 = 0; idx2 < 4; idx2++)
 			{
 				if(*(format + (idx + 1)) == *(option[idx2].data))
