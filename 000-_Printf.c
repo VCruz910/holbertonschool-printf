6ca37ee3506e5ec1a949669feaddfb2424d90fc8
VCruz910 <vscruz910@gmail.com>
2022-07-13 02:27:46 +0000

    Fixed errors

--- 000-_Printf.c
+++ 000-_Printf.c
@@ -23,16 +23,16 @@ int _printf(const char *format, ...)
 	/*Codes made by Victor*/
 	/*Start of our function*/
 	va_start(ptr, format);
-	if (format == NULL || (format [0] == '%' && format[1] == '\0'))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	for (idx = 0; *(format + idx) != '\0'; idx++)
 	{
 		if (format[idx] == '%')
 		{
-			for(idx2 = 0; idx2 < 4; idx2++)
+			for (idx2 = 0; idx2 < 4; idx2++)
 			{
-				if(*(format + (idx + 1)) == *(option[idx2].data))
+				if (*(format + (idx + 1)) == *(option[idx2].data))
 				{
 					option[idx2].f(ptr);
 				}
