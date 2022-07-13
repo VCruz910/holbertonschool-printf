8bb954c96d514f5e743b0b3bae6d9b8e91f2a459
VCruz910 <vscruz910@gmail.com>
2022-07-13 00:25:17 +0000

    Fixing project

--- 000-_Printf.c
+++ 000-_Printf.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * get_escoge - Chooses what type to print.
+ * @data: Character for the data to print.
+ * @ptr: Variatic arguments.
+ * @spaces: spaces for printing.
+ * return: 0
+ **/
+int get_escoge(char data, va_list ptr, int spaces)
+{
+	escoge_t option[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_integer},
+		{'i', print_integer},
+		{'\0', NULL}
+	};
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: Character pointer to the format.
  * Return: (COUNT).
@@ -10,31 +28,22 @@ int _printf(const char *format, ...)
 	/*Variables*/
 	/*Var names by Angeira*/
 	va_list ptr;
-	unsigned int idx = 0, idx2 = 0;
-
-	/*escoge_t made by Angeira*/
-	escoge_t option[] = {
-		{'c', print_char},
-		{'s', print_int},
-		{'d', print_num},
-		{'i', print_num},
-		{'\0', NULL}
-	};
+	int (*f)(va_list ptr);
+	int idx = 0, idx2 = 0;
 
 	/*Codes made by Victor*/
 	/*Start of our function*/
 	va_start(ptr, format);
-	if (ptr == NULL || format == NULL)
-		return (-1);
-	if (format[0] == '%' && format[1] == '\0')
+	if (format == NULL || (format [0] == '%' && format[1] == '\0'))
 		return (-1);
 	/**
 	 *for (idx = 0, idx2 = 0; format[idx] != NULL && idx2 < 4; idx++, idx2++)
 	 *{
 	 *	if(format[idx] == option[idx2].data)
-	 *		option[idx2].F;
+	 *		option[idx2].f;
 	 *}
 	 **/
+	
 
 	/*Checks for strings*/
 	while (format[idx] == option[idx2].data)
@@ -44,11 +53,11 @@ int _printf(const char *format, ...)
 		{
 			if (format[idx] == option[idx2].data)
 				if (format[idx + 1] != '\0')
-					printf(", ");
-			idx2++
+					_putchar(", ");
+			idx2++;
 		}
 		idx++;
 	}
-	printf("\n");
+	_putchar("\n");
 	va_end(ptr);
 }
